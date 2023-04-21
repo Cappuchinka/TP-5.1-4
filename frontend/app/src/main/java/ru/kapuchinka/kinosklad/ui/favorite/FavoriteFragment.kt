24f956120e54f260f47870b3d43a8b87dbc4c1dc
Cappuchinka <volchenko.coffee@gmail.com>
2023-04-21 18:50:51 +0300

    End set bottom navigation bar. Set icon app.

--- frontend/app/src/main/java/ru/kapuchinka/kinosklad/ui/favorite/FavoriteFragment.kt
+++ frontend/app/src/main/java/ru/kapuchinka/kinosklad/ui/favorite/FavoriteFragment.kt
@@ -1,4 +1,4 @@
-package ru.kapuchinka.kinosklad.ui.notifications
+package ru.kapuchinka.kinosklad.ui.favorite
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import ru.kapuchinka.kinosklad.databinding.FragmentNotificationsBinding
+import ru.kapuchinka.kinosklad.databinding.FragmentFavoriteBinding
 
-class NotificationsFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentFavoriteBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val favoriteViewModel =
+            ViewModelProvider(this).get(FavoriteViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        favoriteViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- frontend/app/src/main/java/ru/kapuchinka/kinosklad/ui/favorite/FavoriteViewModel.kt
+++ frontend/app/src/main/java/ru/kapuchinka/kinosklad/ui/favorite/FavoriteViewModel.kt
@@ -1,13 +1,13 @@
-package ru.kapuchinka.kinosklad.ui.dashboard
+package ru.kapuchinka.kinosklad.ui.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavoriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is favorite Fragment"
     }
     val text: LiveData<String> = _text
 }
