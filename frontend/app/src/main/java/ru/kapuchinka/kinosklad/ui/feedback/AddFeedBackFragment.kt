8c72edf30ad31a6c00a2021a11c94ff213a3aa63
Cappuchinka <volchenko.coffee@gmail.com>
2023-04-23 23:40:01 +0300

    Add action for button on feedback

--- frontend/app/src/main/java/ru/kapuchinka/kinosklad/ui/feedback/AddFeedBackFragment.kt
+++ frontend/app/src/main/java/ru/kapuchinka/kinosklad/ui/feedback/AddFeedBackFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import ru.kapuchinka.kinosklad.R
 
 class AddFeedBackFragment : Fragment() {
@@ -13,8 +15,15 @@ class AddFeedBackFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_feed_back, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_feed_back, container, false)
+
+        val publicFeedBackButton : Button = view.findViewById(R.id.button_public_feedback)
+
+        publicFeedBackButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_addFeedBackFragment_to_filmPageFragment)
+        }
+
+        return view
     }
 
 }
