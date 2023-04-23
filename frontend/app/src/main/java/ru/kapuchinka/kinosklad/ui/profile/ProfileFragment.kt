b833ce18ccbfdda6a5cb9ad95bb1f462b6b24e71
Cappuchinka <volchenko.coffee@gmail.com>
2023-04-23 22:39:34 +0300

    Make auth fragments

--- frontend/app/src/main/java/ru/kapuchinka/kinosklad/ui/profile/ProfileFragment.kt
+++ frontend/app/src/main/java/ru/kapuchinka/kinosklad/ui/profile/ProfileFragment.kt
@@ -4,25 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import ru.kapuchinka.kinosklad.R
 import ru.kapuchinka.kinosklad.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val logoutButton : Button = view.findViewById(R.id.button_logout)
+
+        logoutButton.setOnClickListener{
+            it.findNavController().navigate(R.id.action_navigation_profile_to_authFragment)
+        }
+
+        return view
     }
 
 }
