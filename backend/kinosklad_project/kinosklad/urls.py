d7fbfe1fb12f60325d8aeeab37e67a991ab098f3
Cappuchinka <volchenko.coffee@gmail.com>
2023-05-23 00:34:13 +0300

    Edit test register. Add test auth.

--- backend/kinosklad_project/kinosklad/urls.py
+++ backend/kinosklad_project/kinosklad/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_user)
+    path('register/', views.register_user),
+    path('authorize/', views.auth_user),
+    path('add_category/', views.add_category)
 ]
--- backend/kinosklad_project/kinosklad/views.py
+++ backend/kinosklad_project/kinosklad/views.py
@@ -3,6 +3,7 @@ import json
 import firebase
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 config = json.load(open('kinosklad/kinosklad_config.json'))
 app = firebase.initialize_app(config)
@@ -12,7 +13,20 @@ database = app.database()
 
 @api_view(['POST'])
 def register_user(request):
+    email = 'www@gmail.com'
+    passwd = '123qweasd'
+    nickname = 'name'
+    user = auth.create_user_with_email_and_password(email, passwd)
+    user = auth.update_profile(user.get('idToken'), display_name=nickname)
+    return JsonResponse(user)
+
+@api_view(['POST'])
+def auth_user(request):
     email = 'www4@gmail.com'
     passwd = '123qweasd'
-    auth.create_user_with_email_and_password(email, passwd)
-    return HttpResponse(HttpResponse.status_code)
+    user = auth.sign_in_with_email_and_password(email, passwd)
+    return JsonResponse(user)
+
+# @api_view(['POST'])
+# def add_category(request):
+#
