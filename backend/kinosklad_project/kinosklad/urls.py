047f5913a58a8c61ed380f292e1a4224d83f6737
Cappuchinka <volchenko.coffee@gmail.com>
2023-05-25 13:12:44 +0300

    Add test function add category.

--- backend/kinosklad_project/kinosklad/urls.py
+++ backend/kinosklad_project/kinosklad/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('register/', views.register_user),
     path('authorize/', views.auth_user),
-    path('add_category/', views.add_category)
+    path('add_category/', views.add_category),
+    path('get_categories/', views.get_categories),
+    path('add_film/', views.add_film),
+    path('get_films/', views.get_films)
 ]
--- backend/kinosklad_project/kinosklad/views.py
+++ backend/kinosklad_project/kinosklad/views.py
@@ -4,6 +4,7 @@ import firebase
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
 from django.http import JsonResponse
+from datetime import datetime
 
 config = json.load(open('kinosklad/kinosklad_config.json'))
 app = firebase.initialize_app(config)
@@ -27,6 +28,17 @@ def auth_user(request):
     user = auth.sign_in_with_email_and_password(email, passwd)
     return JsonResponse(user)
 
-# @api_view(['POST'])
-# def add_category(request):
-#
+@api_view(['POST'])
+def add_category(request):
+    category_id = 0
+    try:
+        all_categories = database.child("categories").get().val()
+        category_id = len(all_categories)
+    except TypeError:
+        pass
+    name = 'category3'
+
+    category_info = {"name": name}
+
+    database.child("categories").child(category_id).set(category_info)
+    return JsonResponse(database.child("categories").get().val()[category_id])
